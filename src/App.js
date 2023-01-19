7713ae311ff93f27e5a857173b50eee127ed17e0
Sarah-Katz <Sarahkatz59@gmail.com>
2023-01-19 16:42:53 +0100

    Ajout images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //State
+    const [images] = useState([
+        { id: 1, src: "/images/1.jpg" },
+        { id: 2, src: "/images/2.jpg" },
+        { id: 3, src: "/images/3.jpg" },
+        { id: 4, src: "/images/4.jpg" },
+        { id: 5, src: "/images/5.jpg" },
+        { id: 6, src: "/images/6.jpg" }
+      ]);
+ const [currentImageId, setCurrentImagesId] = useState(0);
+    //Comportements
+
+
+    //Render
+    return (
+        <div id="slider">
+            <img src={images[currentImageId].src} alt=""></img>
+        </div>
+    )
 }
 
 export default App;
