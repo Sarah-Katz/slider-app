c04e2b1683f63613290b96eb5f97d14a8a274cc3
Sarah-Katz <Sarahkatz59@gmail.com>
2023-01-23 09:52:38 +0100

    Added comments

--- src/components/Puces/Puces.jsx
+++ src/components/Puces/Puces.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 function Puces({ images, currentImage, onClick, className }) {
+  // map through the images array passed as a prop
+  // for each image, return a div with the class "puce" and an additional class "puce-active" if the currentImage index matches the index of the image in the array
+  // also add an onClick event that calls the onClick function passed as a prop and passes the image's id as an argument
+  // add a key prop for each div for better performance
   return (
     <div className={className}>
       {images.map((image, i) => (
